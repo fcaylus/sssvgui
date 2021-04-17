141350ab4674c5f4154b5acaf70793ce962b31ac
Fabien Caylus <fabien.caylus@gmail.com>
2021-04-17 11:02:27 +0200

    Fix formidable form handling

--- pages/api/optimize.ts
+++ pages/api/optimize.ts
@@ -13,30 +13,29 @@ export const config = {
 export default (req: NextApiRequest, res: NextApiResponse) => {
     return new Promise(async (resolve, reject) => {
         const form = new IncomingForm({});
-        form
-            .on('file', (name: string, file: File) => {
-                const data = fs.readFileSync(file.path).toString('utf8');
-                optimizeSVG(file.name, data, {
-                    crop: true,
-                    viewBox: {
-                        x: 0,
-                        y: 0,
-                        height: 1000,
-                        width: undefined
-                    }
-                }).then((optimized) => {
-                    resolve(res.status(200).json({
-                        svg: Buffer.from(optimized).toString('base64')
-                    }));
-                }).catch((err) => {
-                    console.error(err);
-                    reject(res.status(500).send(err.message !== undefined ? err.message : err));
-                });
-            })
-            .on('aborted', () => {
-                reject(res.status(500).send('Aborted'));
+        form.on('file', (name: string, file: File) => {
+            const data = fs.readFileSync(file.path).toString('utf8');
+            optimizeSVG(file.name, data, {
+                crop: true,
+                viewBox: {
+                    x: 0,
+                    y: 0,
+                    height: 1000,
+                    width: undefined
+                }
+            }).then((optimized) => {
+                resolve(res.status(200).json({
+                    svg: Buffer.from(optimized).toString('base64')
+                }));
+            }).catch((err) => {
+                console.error(err);
+                reject(res.status(500).send(err.message !== undefined ? err.message : err));
             });
+        });
+        form.on('aborted', () => {
+            reject(res.status(500).send('Aborted'));
+        });
 
-        await form.parse(req);
+        await form.parse(req, null);
     });
 }
